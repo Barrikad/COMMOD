599138ba0c9f9566b5f24b4aa97910ee35d07d5c
EmRyom <61932752+EmRyom@users.noreply.github.com>
2020-03-19 07:40:27 +0100

    Add files via upload

--- PrintPG.fs
+++ PrintPG.fs
@@ -2,8 +2,6 @@ module PrintPG
 
 open TypeAST
 
-open progGraph
-
 type Action =
     | BoolAct of Boolean
     | ArithmAct of Arithm
@@ -17,18 +15,18 @@ type nodes =
 
 type Edge =
     | S of (nodes*Action*nodes)
-
+    
 let rec arithm2str (x : Arithm) =
     match x with 
     | Int (x) -> (string x)
     | Float (x) -> (string x)
     | Val (x) -> (string x)
     | ArrVal (a,b) -> a + "[" + arithm2str(b) + "]" 
-    | Plus (a,b) -> arithm2str (a) + " + " + arithm2str (b)
-    | Minus (a,b) -> arithm2str (a) + " - " + arithm2str (b)
-    | Times (a,b) -> arithm2str (a) + " * " + arithm2str (b)
-    | Div (a,b) -> arithm2str (a) + " / " + arithm2str (b)
-    | Neg (x) -> " -" + arithm2str (x)
+    | Plus (a,b) -> arithm2str (a) + "+" + arithm2str (b)
+    | Minus (a,b) -> arithm2str (a) + "-" + arithm2str (b)
+    | Times (a,b) -> arithm2str (a) + "*" + arithm2str (b)
+    | Div (a,b) -> arithm2str (a) + "/" + arithm2str (b)
+    | Neg (x) -> "-" + arithm2str (x)
     | ParA (x) -> "(" + arithm2str(x) + ")"
     | Pow (a,b) -> arithm2str (a) + "^" + arithm2str (b)
     | AError a-> failwith "strange, an AError at this point?"
@@ -41,18 +39,18 @@ let rec bool2str (x : Boolean) =
     | Or (a,b) -> bool2str (a) + " | " + bool2str (b)
     | AndSC (a,b) -> bool2str (a) + " && " + bool2str (b)
     | OrSC (a,b) -> bool2str (a) + " || " + bool2str (b)
-    | Equal (a,b) -> arithm2str (a) + " == " + arithm2str (b)
-    | Greater (a,b) -> arithm2str (a) + " > " + arithm2str (b)
-    | Lesser (a,b) -> arithm2str (a) + " < " + arithm2str (b)
-    | Not (a) -> " !" + bool2str(a) 
+    | Equal (a,b) -> arithm2str (a) + "==" + arithm2str (b)
+    | Greater (a,b) -> arithm2str (a) + ">" + arithm2str (b)
+    | Lesser (a,b) -> arithm2str (a) + "<" + arithm2str (b)
+    | Not (a) -> "!" + bool2str(a) 
     | ParB (x) -> "(" + bool2str(x) + ")"
     | BError a-> failwith "strange, an BError at this point?"
 
 let rec Command2str (x : Command) =
     match x with
-    | Assign (a,b) -> a + " := " + arithm2str(b)
-    | ArrAssign (a,b,c) -> a + "[" + arithm2str(b) + "] := " + arithm2str(c) 
-    | Skip -> " skip "
+    | Assign (a,b) -> a + ":=" + arithm2str(b)
+    | ArrAssign (a,b,c) -> a + "[" + arithm2str(b) + "]:=" + arithm2str(c) 
+    | Skip -> "skip"
     | Coms (a,b) -> Command2str(a) + " " + Command2str(b)
     | If (x) -> GCommand2str(x)
     | Do (x) -> GCommand2str(x)
@@ -71,10 +69,15 @@ let act2str = function
 
 let toDOT (x) =
     match x with
-    | S (Start,b,End) -> "q▷ -> q◀ [label = \"" + act2str(b) + "\"];" 
-    | S (Start,b,c) ->  "q▷ -> q" + (string c) + " [label = \"" + act2str(b) + "\"];" 
-    | S (a,b,End) -> "q" + (string a) + " -> q◀ [label = \"" + act2str(b) + "\"];" 
-    | S (a,b,c) ->  "q" + (string a) + " -> q" + (string c) + " [label = \"" + act2str(b) + "\"];" 
+    | S (Start,b,End) -> 
+        "q▷ -> q◀ [label = \"" + act2str(b) + "\"];" 
+    | S (Start,b,Node c) ->  
+        "q▷ -> q" + string(c) + " [label = \"" + act2str(b) + "\"];" 
+    | S (Node a,b,End) -> 
+        "q" + string(a) + " -> q◀ [label = \"" + act2str(b) + "\"];" 
+    | S (Node a,b,Node c) ->  
+        "q" + string(a) + " -> q" + string(c) + " [label = \"" + act2str(b) + "\"];" 
+    | _ -> failwith "Incorrect Edges"
 
 let rec edge2string (l : Edge list) =
     match l with 
@@ -83,5 +86,4 @@ let rec edge2string (l : Edge list) =
 
 let toList s = Set.fold (fun l se -> se::l) [] s
 
-let printPG (l : Set<Edge>) = printf "digraph PG {rankdir=LR; \n 
-node [shape = doublecircle]; q◀; \n node [shape = circle]; q▷ ; \n %s }" (edge2string (toList(l))) |> ignore
+let printPG (l : Set<Edge>) = printf "digraph PG {rankdir=LR; \nnode [shape = doublecircle]; q◀; \nnode [shape = circle]; q▷ ; \n%s}" (edge2string (toList(l))) |> ignore  
