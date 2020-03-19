5767d577ea7b7938ad928a5acc7d45f0fd041010
Barrikad <s184459@student.dtu.dk>
2020-03-19 09:45:57 +0100

    just chillin

--- GCP.fsx
+++ GCP.fsx
@@ -4,20 +4,19 @@ open FSharp.Text.Lexing
 open System
 
 #load "TypeAST.fs"
-
 open TypeAST
 
 #load "PrintAST.fs"
-
 open PrintAST
 
 #load "GCPParser.fs"
-
 open GCPParser
 
 #load "GCPLexer.fs"
-
 open GCPLexer
+
+#load "progGraph.fs"
+open progGraph
 
 let program = IO.File.ReadAllText "GCProgram.txt"
 
--- progGraph.fs
+++ progGraph.fs
@@ -9,6 +9,12 @@ type nodes =
     | End
     | Node of int
 
+type Action =
+    | BoolAct of Boolean
+    | ArithmAct of Arithm
+    | CommandAct of Command
+    | GCommandAct of GCommand
+
 let rec appendNode node = 
     match node with
     | Start -> Node(0)
@@ -21,20 +27,20 @@ let rec appendNode node =
 
 let rec guardedCommands currNode nextNode out = function
     | Gives(guard, command) -> let x = appendNode currNode
-                               let y = Set.add (currNode, Boolean(guard), x) out
+                               let y = Set.add (currNode, BoolAct(guard), x) out
                                commands x nextNode y command
     | Else(gcom1,gcom2) -> let z = guardedCommands currNode nextNode out gcom1
                            guardedCommands currNode nextNode z gcom2
 
 and commands currNode nextNode out = function
-    | Assign(str, arithm) -> Set.add (currNode, (Assign(str,arithm)), nextNode) out
-    | ArrAssign(str,arithm1,arithm2) -> Set.add(currNode,(ArrAssign(str,arithm1,arithm2)),nextNode) out
-    | Skip -> Set.add(currNode,(Skip),nextNode) out
+    | Assign(str, arithm) -> Set.add (currNode, CommandAct(Assign(str,arithm)), nextNode) out
+    | ArrAssign(str,arithm1,arithm2) -> Set.add(currNode,CommandAct(ArrAssign(str,arithm1,arithm2)),nextNode) out
+    | Skip -> Set.add(currNode,CommandAct(Skip),nextNode) out
     | Coms(command1, command2) -> let x = appendNode currNode
                                   let holder = commands currNode x out command1
                                   commands x nextNode holder command2
     | If(gcom) -> guardedCommands currNode nextNode out gcom
     | Do(gcom)-> let (bool,com) = gcom
                  let holder = guardedCommands currNode currNode out gcom
-                 Set.add (currNode,Boolean(Not bool),nextNode) holder
+                 Set.add (currNode,BoolAct(Not bool),nextNode) holder
                
